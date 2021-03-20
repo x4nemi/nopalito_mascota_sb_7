6a5a99985ab29094022b783ccf3b52cdf90ad9f6
Ximena <ximena.garcia7482@alumnos.udg.mx>
2021-03-19 22:50:27 -0600

    ejemplos insert

--- nopalito.sql
+++ nopalito.sql
@@ -25,3 +25,13 @@ CREATE TABLE arbol(
     lugarId INT UNSIGNED NOT NULL,
     FOREIGN KEY (lugarId) REFERENCES lugar(id)
 );
+
+INSERT INTO usuario (correo, contrasenia) VALUES ("hola@hotmail.com", "QwerT1_");
+INSERT INTO usuario (correo, contrasenia) VALUES ("estudiante@alumnos.udg.mx", "w0lA!")
+
+INSERT INTO lugar(codigo_postal, municipio, colonia, calle, usuarioId) VALUES (36260, "Guanajuato", "Santa Teresa", "Buenos Aires", LAST_INSERT_ID());
+INSERT INTO lugar(codigo_postal, municipio, colonia, calle, usuarioId) VALUES (44100, "Guadalajara", "Guadalajara Centro", "Hospital", 1);
+
+INSERT INTO arbol(fecha, especie, imagen, lugarId) VALUES (2020-06-23, "Abedul", "usuario\docs\arbol01.jpg", 2);
+INSERT INTO arbol(fecha, especie, imagen, lugarId) VALUES (2020-10-12, "Jacaranda", "usuario\docs\arbol02.jpg", 1);
+INSERT INTO arbol(fecha, especie, imagen, lugarId) VALUES (2021-01-22, "Pino", "usuario\docs\arbol03.png", 2);
